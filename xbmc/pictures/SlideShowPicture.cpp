fa4f5e2e2e1432a369d9ddb2f42e722ea671c4e8
h.udo <hudokkow@gmail.com>
2017-07-12 11:19:55 +0100

    [modernize][pictures] Prefer 'nullptr' usage

--- xbmc/pictures/SlideShowPicture.cpp
+++ xbmc/pictures/SlideShowPicture.cpp
@@ -45,7 +45,7 @@ static float zoomamount[10] = { 1.0f, 1.2f, 1.5f, 2.0f, 2.8f, 4.0f, 6.0f, 9.0f, 
 
 CSlideShowPic::CSlideShowPic() : m_alpha(0)
 {
-  m_pImage = NULL;
+  m_pImage = nullptr;
   m_bIsLoaded = false;
   m_bIsFinished = false;
   m_bDrawNextImage = false;
@@ -54,7 +54,7 @@ CSlideShowPic::CSlideShowPic() : m_alpha(0)
   m_bCanMoveHorizontally = false;
   m_bCanMoveVertically = false;
 #ifdef HAS_DX
-  m_vb = NULL;
+  m_vb = nullptr;
 #endif
 }
 
@@ -69,7 +69,7 @@ void CSlideShowPic::Close()
   if (m_pImage)
   {
     delete m_pImage;
-    m_pImage = NULL;
+    m_pImage = nullptr;
   }
   m_bIsLoaded = false;
   m_bIsFinished = false;
@@ -257,7 +257,7 @@ void CSlideShowPic::UpdateTexture(CBaseTexture* pTexture)
   if (m_pImage)
   {
     delete m_pImage;
-    m_pImage = NULL;
+    m_pImage = nullptr;
   }
   m_pImage = pTexture;
   m_fWidth = (float)pTexture->GetWidth();
@@ -749,9 +749,9 @@ void CSlideShowPic::Render()
   // now render the image in the top right corner if we're zooming
   if (m_fZoomAmount == 1.0f || m_bIsComic) return ;
 
-  Render(m_bx, m_by, NULL, PICTURE_VIEW_BOX_BACKGROUND);
+  Render(m_bx, m_by, nullptr, PICTURE_VIEW_BOX_BACKGROUND);
   Render(m_sx, m_sy, m_pImage, 0xFFFFFFFF);
-  Render(m_ox, m_oy, NULL, PICTURE_VIEW_BOX_COLOR);
+  Render(m_ox, m_oy, nullptr, PICTURE_VIEW_BOX_COLOR);
 }
 
 #ifdef HAS_DX
@@ -804,7 +804,7 @@ void CSlideShowPic::Render(float *x, float *y, CBaseTexture* pTexture, color_t c
     vertex[1].texCoord.x = vertex[2].texCoord.x = 1.0f;
     vertex[2].texCoord.y = vertex[3].texCoord.y = 1.0f;
   }
-  vertex[4] = vertex[0]; // Not used when pTexture != NULL
+  vertex[4] = vertex[0]; // Not used when pTexture != nullptr
 
   CGUIShaderDX* pGUIShader = g_Windowing.GetGUIShader();
   pGUIShader->Begin(SHADER_METHOD_RENDER_TEXTURE_BLEND);
@@ -981,6 +981,6 @@ void CSlideShowPic::Render(float *x, float *y, CBaseTexture* pTexture, color_t c
   g_Windowing.DisableGUIShader();
 #else
 // SDL render
-  g_Windowing.BlitToScreen(m_pImage, NULL, NULL);
+  g_Windowing.BlitToScreen(m_pImage, nullptr, nullptr);
 #endif
 }
