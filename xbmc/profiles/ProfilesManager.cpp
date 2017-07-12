8bcbe2903f9c63a63450c46bb05d14edc357c2d9
h.udo <hudokkow@gmail.com>
2017-07-12 11:19:56 +0100

    [modernize][profiles] Prefer 'nullptr' usage

--- xbmc/profiles/ProfilesManager.cpp
+++ xbmc/profiles/ProfilesManager.cpp
@@ -205,7 +205,7 @@ bool CProfilesManager::Save(const std::string &file) const
   CXBMCTinyXML xmlDoc;
   TiXmlElement xmlRootElement(XML_PROFILES);
   TiXmlNode *pRoot = xmlDoc.InsertEndChild(xmlRootElement);
-  if (pRoot == NULL)
+  if (pRoot == nullptr)
     return false;
 
   XMLUtils::SetInt(pRoot, XML_LAST_LOADED, m_currentProfile);
@@ -302,11 +302,11 @@ bool CProfilesManager::DeleteProfile(size_t index)
 {
   CSingleLock lock(m_critical);
   const CProfile *profile = GetProfile(index);
-  if (profile == NULL)
+  if (profile == nullptr)
     return false;
 
   CGUIDialogYesNo* dlgYesNo = g_windowManager.GetWindow<CGUIDialogYesNo>(WINDOW_DIALOG_YES_NO);
-  if (dlgYesNo == NULL)
+  if (dlgYesNo == nullptr)
     return false;
 
   std::string str = g_localizeStrings.Get(13201);
@@ -387,7 +387,7 @@ const CProfile* CProfilesManager::GetProfile(size_t index) const
   if (index < m_profiles.size())
     return &m_profiles[index];
 
-  return NULL;
+  return nullptr;
 }
 
 CProfile* CProfilesManager::GetProfile(size_t index)
@@ -396,7 +396,7 @@ CProfile* CProfilesManager::GetProfile(size_t index)
   if (index < m_profiles.size())
     return &m_profiles[index];
 
-  return NULL;
+  return nullptr;
 }
 
 int CProfilesManager::GetProfileIndex(const std::string &name) const
